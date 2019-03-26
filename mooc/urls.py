1ca081427fe5117e3e9b371007654f8e20465921
Nthan <kuaile86@qq.com>
2019-03-26 21:53:23 +0800

    利用现成的前端文件，完成了网站主页和登录页面初始配配置

--- mooc/urls.py
+++ mooc/urls.py
@@ -17,6 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 import xadmin
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
+    url('^$', TemplateView.as_view(template_name="index.html"), name="index"),
+    url('^login/$', TemplateView.as_view(template_name="login.html"), name="login")
+
 ]
