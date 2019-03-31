d6e1aa0fd79fdc0655582eb28a55514e859f57f8
Nthan <kuaile86@qq.com>
2019-03-31 14:23:40 +0800

    利用Ajax完成课程机构的收藏功能，课程机构模块暂时告一段落，等后续其他模块完成后在进行联动

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -3,15 +3,19 @@ __author__ = "hudingjing"
 __date__ = '2019/3/29 16:28 '
 
 from django.conf.urls import url
-from organization.views import OrgView,UserAskView,OrgHome,OrgCourse,OrgTeacher,OrgDesc
+from organization.views import OrgView,UserAskView,OrgHome,OrgCourse,OrgTeacher,OrgDesc,FavorView
 
 urlpatterns = [
     # 课程机构列表url
     url(r'list/$', OrgView.as_view(), name="org_list"),
+    #课程咨询
     url(r'add_ask/$',UserAskView.as_view(),name='add_ask'),
+    #课程详情页
     url(r'home/(?P<org_id>\d+)/$',OrgHome.as_view(),name='org_home'),
     url(r'course/(?P<org_id>\d+)/$', OrgCourse.as_view(), name='org_course'),
     url(r'teacher/(?P<org_id>\d+)/$', OrgTeacher.as_view(), name='org_teacher'),
     url(r'desc/(?P<org_id>\d+)/$', OrgDesc.as_view(), name='org_desc'),
+    #用户收藏，包括机构、课程、讲师
+    url(r'favor/$', FavorView.as_view(), name="favor"),
 
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -5,6 +5,7 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 
 from .models import CourseOrg, CityDict
+from operation.models import UserFavorite
 from .forms import UserAskForm
 
 
@@ -72,11 +73,15 @@ class OrgHome(View):
         all_courses = org.course_set.all()[:4]
         all_teachers = org.teacher_set.all()[:3]
         current_page='home'
+        has_favor=False
+        if request.user.is_authenticated and UserFavorite.objects.filter(user=request.user,fav_type=2,fav_id=org_id):
+            has_favor=True
         return render(request, 'org-detail-homepage.html', {
             "all_courses": all_courses,
             "all_teachers": all_teachers,
             'org':org,
-            'current_page':current_page
+            'current_page':current_page,
+            'has_favor':has_favor
         })
 
 
@@ -87,12 +92,15 @@ class OrgCourse(View):
         org = CourseOrg.objects.get(id=int(org_id))
         all_courses = org.course_set.all()[:4]
         current_page = 'course'
+        has_favor=False
+        if request.user.is_authenticated and UserFavorite.objects.filter(user=request.user,fav_type=2,fav_id=org_id):
+            has_favor=True
 
         return render(request, 'org-detail-course.html', {
             "all_courses": all_courses,
             'org': org,
-            'current_page': current_page
-
+            'current_page': current_page,
+            'has_favor': has_favor
         })
 
 
@@ -103,11 +111,15 @@ class OrgTeacher(View):
         org = CourseOrg.objects.get(id=int(org_id))
         all_teachers = org.teacher_set.all()[:3]
         current_page = 'teacher'
+        has_favor=False
+        if request.user.is_authenticated and UserFavorite.objects.filter(user=request.user,fav_type=2,fav_id=org_id):
+            has_favor=True
 
         return render(request, 'org-detail-teachers.html', {
             "all_teachers": all_teachers,
             'org': org,
-            'current_page': current_page
+            'current_page': current_page,
+            'has_favor': has_favor
 
         })
 
@@ -118,9 +130,56 @@ class OrgDesc(View):
     def get(self, request, org_id):
         org = CourseOrg.objects.get(id=int(org_id))
         current_page = 'desc'
+        has_favor=False
+        if request.user.is_authenticated and UserFavorite.objects.filter(user=request.user,fav_type=2,fav_id=org_id):
+            has_favor=True
 
         return render(request, 'org-detail-desc.html', {
             'org': org,
-            'current_page': current_page
+            'current_page': current_page,
+            'has_favor': has_favor
 
         })
+
+
+class FavorView(View):
+    """
+    用户收藏或取消收藏机构、课程、讲师
+    """
+    def post(self,request):
+        #从前台ajax提取的数据，默认值为0，是以防空串转int出错
+        favor_id=request.POST.get('fav_id','0')
+        favor_type=request.POST.get('fav_type','0')
+
+        #django提供了一个内置user，用于判断用户是否登录
+        if not request.user.is_authenticated:
+            #用户未登录时返回给前台ajax的json数据
+            return HttpResponse(
+                "{'status': 'fail', 'msg':'用户未登录'}",
+                content_type='application/json')
+
+        exist_records=UserFavorite.objects.filter(user=request.user,fav_id=int(favor_id),fav_type=int(favor_type))
+
+        if exist_records:
+            #记录存在，表示用户取消收藏，删除记录并将收藏按钮要变成收藏
+            exist_records.delete()
+            return HttpResponse(
+                "{'status': 'success', 'msg':'收藏'}",
+                content_type='application/json')
+        else:
+            #由于默认就是0，所以需要判断
+            if favor_type>'0'and favor_id>'0':
+                user_favor=UserFavorite(user=request.user,fav_id=int(favor_id),fav_type=int(favor_type))
+                user_favor.save()
+                return HttpResponse(
+                    "{'status': 'success', 'msg':'已收藏'}",
+                    content_type='application/json')
+            else:
+                return HttpResponse(
+                    "{'status': 'fail', 'msg':'收藏出错'}",
+                    content_type='application/json')
+
+
+
+
+
